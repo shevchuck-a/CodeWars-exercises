29cf318dcf24f133bf63dbc7c69e18433ebe1e01
Alexander Shevchuck <shevchuck.a@gmail.com>
2019-08-07 23:32:48 +0300

    Some exercises

---
+++ Exes and Ohs/Exes and Ohs/Exes and Ohs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exes_and_Ohs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParsingAndCount();
+            Console.ReadLine();
+        }
+        static void ParsingAndCount()
+        {
+            Console.WriteLine("Please, write some count of 'x's and 'o's.");
+            string userInput = Console.ReadLine();
+            string.TryParse(string, out string userInput);
+            Console.WriteLine(userInput);
+        }
+    }
+}
---
+++ Find the divisors!/Find the divisors!/Find the divisors!/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Find_the_divisors_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
